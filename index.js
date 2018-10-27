6e39bb8e5aa31277af6a3565c02428da0d4eceac
Vadym <vzhufk@gmail.com>
2018-10-27 16:06:35 +0300

    [imp] Initial lib.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const search = require("./search");
+const fs = require("fs");
 
-console.log(search("UA", v => v.zip_code == "59330"));
+const CODES = fs.readdirSync(testFolder).map(f => f.replace(".js", ""));
+
+const z1p = codes => {
+  if (!codes) {
+    codes = CODES;
+  }
+
+  const validate = codes.every(c => CODES.includes(c));
+
+  if (!validate) {
+    throw new Error(
+      "Some of county codes are not supported. Provide valid codes."
+    );
+  }
+
+  const methods = {
+    raw: comp => CODES.reduce((a, v) => [...a, search(v, comp)])
+  };
+
+  return methods;
+};
+
+module.exports = z1p;
